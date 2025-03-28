494efcff648cdaddf1066fac511991db521b039d
fuureya <agiljibrin009@gmail.com>
2025-03-28 10:29:26 +0800

    feat: initial layout auth

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Livewire\Homepage;
 use App\Livewire\Uoms as UomsRoute;
 use App\Livewire\Categories as CategoryRoute;
 use App\Livewire\Inventory as inventoryRoutes;
+use App\Livewire\Login;
 use App\Livewire\Suppliers as SupplierRoutes;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,4 @@ Route::get('suppliers', SupplierRoutes::class);
 Route::get('atur-hak-akses', AturHakAkses::class);
 Route::get('atur-grup', AturGrup::class);
 Route::get('atur-user', AturUser::class);
+Route::get('login', Login::class);

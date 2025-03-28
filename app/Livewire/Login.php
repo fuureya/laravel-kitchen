baf0f75fa437b4320a40444246b62cc519e1a749
fuureya <agiljibrin009@gmail.com>
2025-03-28 13:37:22 +0800

    fix: login

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -3,9 +3,25 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Login extends Component
 {
+    public $username, $password;
+    public function login()
+    {
+        $credential = $this->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credential)) {
+            session()->regenerate();
+            return redirect()->intended('dashboard');
+        } else {
+            session()->flash('error', 'Username atau Password salah.');
+        }
+    }
     public function render()
     {
         return view('livewire.login')->layout('components.layouts.auth');
--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,3 +1,33 @@
-<div>
-    {{-- Stop trying to control. --}}
-</div>
+<section class="vh-100">
+    <div class="container-fluid h-custom">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-md-9 col-lg-6 col-xl-5">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                    class="img-fluid" alt="Sample image">
+            </div>
+            <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                <form wire:submit.prevent='login'>
+                    <div data-mdb-input-init class="form-outline mb-4">
+                        <input type="text" id="username" class="form-control form-control-lg"
+                            placeholder="Enter Username" wire:model='username' />
+                        <label class="form-label" for="username">Username</label>
+                    </div>
+
+                    <div data-mdb-input-init class="form-outline mb-3">
+                        <input type="password" id="password" class="form-control form-control-lg"
+                            placeholder="Enter password" wire:model='password' />
+                        <label class="form-label" for="password">Password</label>
+                    </div>
+
+
+                    <div class="text-center text-lg-start mt-4 pt-2">
+                        <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
+                            style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+</section>
